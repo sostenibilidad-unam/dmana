29055f678fa02f40438c7ae9cddf0e81148a2ecb
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-08-21 11:10:11 -0500

    ai

--- website/nwa/agency_hiveplot.py
+++ website/nwa/agency_hiveplot.py
@@ -1,6 +1,8 @@
 from .models import Person, AgencyEdge, Action, Sector, Category
 import networkx as nx
 from pprint import pprint
+from pyveplot import Hiveplot, Node, Axis
+import operator
 
 
 def agency_hiveplot(queryset):
@@ -10,31 +12,71 @@ def agency_hiveplot(queryset):
     categories = {}
 
     for e in queryset:
-
+        # create NX graph
         g.add_node(e.person, type='person',
                    sector=str(e.person.sector),
                    ego=e.person.ego)
 
+        g.add_node(e.action, type='action', 
+                   category=str(e.action.category))
+
+        g.add_edge(e.person,
+                   e.action,
+                   people=", ".join([str(p)
+                                    for p in e.people.all()]))
+        
         # populate categories dict
         if e.action.category in categories:
             categories[e.action.category].add(e.action)
         else:
             categories[e.action.category] = set([e.action, ])
 
-        g.add_node(e.action, type='action',
-                   category=str(e.action.category))
-
-        # populate sectors dict
+        # populate sectors dict from people in edge
         for p in e.people.all():
+
             if p.sector in sectors:
                 sectors[p.sector].add(p)
             else:
                 sectors[p.sector] = set([p, ])
 
-        g.add_edge(e.person,
-                   e.action,
-                   people=", ".join([str(p)
-                                    for p in e.people.all()]))
+        # add egos to sectors dict
+        if e.person.sector in sectors:
+            sectors[e.person.sector].add(e.person)
+        else:
+            sectors[e.person.sector] = set([e.person.sector, ])
 
-    pprint(categories)
+    # create in-degrees dict
+    in_degree = {v: g.in_degree(v) for v in g.nodes}
+
+    # sort sectors by length
+    sec_len = {sector: len(sectors[sector])
+               for sector in sectors}
+    sorted_sec_len = sorted(sec_len.items(), key=operator.itemgetter(1))
+
+    # our hiveplot object
+    h = Hiveplot( 'agency_hive.svg')
+    offcenter = 10
+    spacer = 10
+    ego_node_size = 10
+    alter_node_size = 5
+    angle = 0
+
+    # create alter axes
+    alter_axes = {}
+    start = offcenter
+    end = 0
+    for sector, sec_len in sorted_sec_len:
+        if sector is None:
+            continue
+        start += spacer + end
+        
+        axis_len = sec_len * alter_node_size
+        end += axis_len
+        
+        alter_axes[sector] = Axis(start=start, end=end,
+                                  angle=angle, stroke="grey")
+
+    h.axes = alter_axes.values()
+
+    h.save()
     return g
