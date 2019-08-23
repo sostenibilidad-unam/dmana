453eba1ff47a27619ceffa4aef6fc809c6774090
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-08-23 12:57:12 -0500

    placement of action nodes

--- website/nwa/agency_hiveplot.py
+++ website/nwa/agency_hiveplot.py
@@ -18,10 +18,10 @@ def agency_hiveplot(queryset):
         g.add_node(e.person, type='person',
                    sector=str(e.person.sector),
                    ego=e.person.ego)
-        
-        g.add_node(e.action, type='action', 
+
+        g.add_node(e.action, type='action',
                    category=str(e.action.category))
-        
+
         for p in e.people.all():
             g.add_node(p, type='person',
                        sector=str(p.sector),
@@ -32,12 +32,12 @@ def agency_hiveplot(queryset):
             # alter to action
             g.add_edge(p,
                        e.action)
-
+        # ego to action
         g.add_edge(e.person,
                    e.action,
                    people=", ".join([str(p)
                                     for p in e.people.all()]))
-        
+
         # populate action categories dict
         if e.action.category in categories:
             categories[e.action.category].add(e.action)
@@ -60,6 +60,7 @@ def agency_hiveplot(queryset):
 
     # create in-degrees dict
     in_degree = {v: g.in_degree(v) for v in g.nodes}
+    sorted_in_degree = sorted(in_degree.items(), key=operator.itemgetter(1))
 
     # sort sectors by length
     sec_len = {sector: len(sectors[sector])
@@ -67,11 +68,10 @@ def agency_hiveplot(queryset):
     sorted_sec_len = sorted(sec_len.items(), key=operator.itemgetter(1))
 
     # our hiveplot object
-    h = Hiveplot( 'agency_hive.svg')
+    h = Hiveplot('agency_hive.svg')
     offcenter = 10
     spacer = 10
-    ego_node_size = 10
-    alter_node_size = 5
+    node_size = 10.0
     angle = 0
 
     # create alter axes
@@ -82,30 +82,47 @@ def agency_hiveplot(queryset):
     for sector, sec_len in sorted_sec_len:
         if sector is None:
             continue
-        axis_len = sec_len * alter_node_size
-        
+        axis_len = sec_len * node_size
+
         end = start + axis_len
-        
+
         alter_axes[sector] = Axis(start=start, end=end,
                                   angle=angle, stroke="grey")
 
         alter_axes_scales[sector] = Scale(
             domain=[start, end],
             range=[0, 1])
-        
-        
-        start = end + spacer        
-    
-    
-    # ego_count = Alter.objects.filter(name__contains='TL0').count()
-    # ego_scale= Scale(domain=[Alter.objects.order_by('degree')[0].degree,
-    #                          Alter.objects.order_by('-degree')[0].degree],
-    #                  range=[5, 30])
+
+        start = end + spacer
+
+    deg_scale = Scale(domain=[1.0, max(in_degree.values())],
+                      range=[1.0, node_size])
+
     h.axes = list(alter_axes.values())
 
-    
+    # create ego axis
     ego_axis = Axis(start=offcenter, end=end,
                     angle=angle - 120, stroke="firebrick")
+    i = 0.5
+    for ego, degree in sorted_in_degree:
+        if type(ego) is not Person:
+            continue
+        if ego.ego is not True:
+            continue
+        node = Node(ego)
+        ego_axis.add_node(node,
+                          i / len(categories[cat]))
+        i += 1.0
+        node.dwg = node.dwg.circle(
+            center=(node.x, node.y),
+            r=0.5 * deg_scale.linear(in_degree[ego]),
+            fill='orange',
+            fill_opacity=0.5,
+            stroke=random.choice(['red',
+                                  'crimson',
+                                  'coral',
+                                  'purple']),
+            stroke_width=0.3)
 
     h.axes.append(ego_axis)
 
@@ -113,24 +130,29 @@ def agency_hiveplot(queryset):
     action_axis = Axis(start=offcenter, end=end,
                        angle=angle + 120, stroke="darkgreen")
 
-    pprint(in_degree)
     # populate action axis
     for cat in categories:
-        i = 0.0
-        for action in categories[cat]:
+        i = 0.5
+        for action, degree in sorted_in_degree:
+            if type(action) is not Action:
+                continue
             node = Node(action)
             action_axis.add_node(node,
                                  i / len(categories[cat]))
-            i += 1
-            node.dwg = node.dwg.circle(center = (node.x, node.y),
-                                       r      = in_degree[action],
-                                       fill   = 'orange',
-                                       fill_opacity = 0.5,
-                                       stroke = random.choice(['red','crimson','coral','purple']),
-                                       stroke_width = 0.3)
-        
+            i += 1.0
+            node.dwg = node.dwg.circle(
+                center=(node.x, node.y),
+                r=0.5 * deg_scale.linear(in_degree[action]),
+                fill='orange',
+                fill_opacity=0.5,
+                stroke=random.choice(['red',
+                                      'crimson',
+                                      'coral',
+                                      'purple']),
+                stroke_width=0.3)
+
     h.axes.append(action_axis)
 
-    
+
     h.save()
     return g
