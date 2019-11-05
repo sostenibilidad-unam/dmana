2795503e3943e3ae82872b6bbcfb4ed0654e592c
rgarcia-herrera <rgarcia@iecologia.unam.mx>
2019-11-05 11:35:54 -0600

    many more colors

--- website/nwa/agency_hiveplot.py
+++ website/nwa/agency_hiveplot.py
@@ -11,11 +11,16 @@ import svgwrite
 # action colors
 ac = ['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c', '#fb9a99',
       '#e31a1c', '#fdbf6f', '#ff7f00', '#cab2d6',
-      '#6a3d9a', '#ffff99', '#b15928']
+      '#6a3d9a', '#ffff99', '#b15928',
+      '#8dd3c7', '#bebada', '#fb8072', '#80b1d3', '#fdb462',
+      '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5',
+      '#ffed6f']
 
 # sector colors
-sc = ['#8dd3c7','#bebada','#fb8072','#80b1d3','#fdb462','#b3de69',
-      '#fccde5','#d9d9d9','#bc80bd','#ccebc5','#ffed6f']
+sc = ['#8dd3c7', '#bebada', '#fb8072', '#80b1d3', '#fdb462',
+      '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5',
+      '#ffed6f', '#8dd3c7', '#bebada', '#fb8072', '#80b1d3', '#fdb462',
+      '#b3de69', '#fccde5', '#d9d9d9', '#bc80bd', '#ccebc5',]
 
 ego_color = '#b2df8a'
 
